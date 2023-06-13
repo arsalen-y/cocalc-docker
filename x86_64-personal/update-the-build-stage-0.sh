bbf8e7563d7cfa96d7567fe3d404dc4f7d54d38e
arsalen-y <arsalen.hagui@yields.io>
2023-06-13 17:36:40 +0200

    #YCE-2308 fix: SSH authorized keys config file

--- x86_64-personal/update-the-build-stage-0.sh
+++ x86_64-personal/update-the-build-stage-0.sh
@@ -12,4 +12,5 @@ echo $commit | cut -c-12 > current_commit
 time docker build --build-arg commit=$commit --build-arg BRANCH=$BRANCH --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') -f ../Dockerfile-personal -t cocalc-personal $@ ..
 docker tag cocalc-personal:latest sagemathinc/cocalc-v2-personal:latest
 docker tag cocalc-personal:latest sagemathinc/cocalc-v2-personal:`cat current_commit`
-docker run --name=cocalc-test-personal -d -p 127.0.0.1:5123:5000 sagemathinc/cocalc-v2-personal
+docker tag cocalc-personal:latest build.yields.io/chiron-enterprise/cocalc-v2-personal:1.0
+docker push build.yields.io/chiron-enterprise/cocalc-v2-personal:1.0
