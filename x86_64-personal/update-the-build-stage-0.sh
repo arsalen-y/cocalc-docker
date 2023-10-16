67f1b8b5fc72bdd8df509aeb43a25084503d49ef
arsalen-y <arsalen.hagui@yields.io>
2023-10-16 18:50:17 +0200

    fix: ls-remote && tag

--- x86_64-personal/update-the-build-stage-0.sh
+++ x86_64-personal/update-the-build-stage-0.sh
@@ -7,10 +7,9 @@ set -ex
 
 export BRANCH="${BRANCH:-master}"
 echo "BRANCH=$BRANCH"
-commit=`git ls-remote -h https://github.com/sagemathinc/cocalc $BRANCH | awk '{print $1}'`
+commit=`git ls-remote -h https://github.com/arsalen-y/cocalc $BRANCH | awk '{print $1}'`
 echo $commit | cut -c-12 > current_commit
 time docker build --build-arg commit=$commit --build-arg BRANCH=$BRANCH --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') -f ../Dockerfile-personal -t cocalc-personal $@ ..
 docker tag cocalc-personal:latest sagemathinc/cocalc-v2-personal:latest
 docker tag cocalc-personal:latest sagemathinc/cocalc-v2-personal:`cat current_commit`
-docker tag cocalc-personal:latest build.yields.io/chiron-enterprise/cocalc-v2-personal:1.0
-docker push build.yields.io/chiron-enterprise/cocalc-v2-personal:1.0
+docker tag cocalc-personal:latest build.yields.io/chiron-enterprise/cocalc-v2-personal:1.1
