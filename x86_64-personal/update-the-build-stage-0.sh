c1ec1d659e7609d23e9b3ab06fa37d7fee3962ca
arsalen-y <arsalen.hagui@yields.io>
2023-10-19 23:30:13 +0200

    #YCE-2505: chore - arbitrary uid/gid (default: 1002280001)

--- x86_64-personal/update-the-build-stage-0.sh
+++ x86_64-personal/update-the-build-stage-0.sh
@@ -9,7 +9,7 @@ export BRANCH="${BRANCH:-master}"
 echo "BRANCH=$BRANCH"
 commit=`git ls-remote -h https://github.com/arsalen-y/cocalc $BRANCH | awk '{print $1}'`
 echo $commit | cut -c-12 > current_commit
-time docker build --build-arg U_ID=1000 --build-arg G_ID=1000 --build-arg commit=$commit --build-arg BRANCH=$BRANCH --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') -f ../Dockerfile-personal -t cocalc-personal $@ ..
+time docker build --build-arg U_ID=1002280001 --build-arg G_ID=1002280001 --build-arg commit=$commit --build-arg BRANCH=$BRANCH --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') -f ../Dockerfile-personal -t cocalc-personal $@ ..
 docker tag cocalc-personal:latest sagemathinc/cocalc-v2-personal:latest
 docker tag cocalc-personal:latest sagemathinc/cocalc-v2-personal:`cat current_commit`
 docker tag cocalc-personal:latest build.yields.io/chiron-enterprise/cocalc-v2-personal:dev
